b057b117893700834751978834a637c0db3081db
Ahmad Masud <ahmadmasud25@hotmail.com>
2022-12-24 22:47:20 -0800

    update build for gh-pages

--- src/Components/Projects/ProjectsComponent.js
+++ src/Components/Projects/ProjectsComponent.js
@@ -1,7 +1,7 @@
 import './ProjectsComponent.css';
 import PaintGif from './Paint.gif'
 import WebsiteGif from './Website.gif'
-import ImagePlaceholder from './placeholder.webp'
+import SortGif from './SortingVisualizer.gif'
 
 function ProjectsComponent() {
   return (
@@ -43,21 +43,19 @@ function ProjectsComponent() {
                 </div>
             </div>
             <div className='project-component'>
-                <img className='project-component-gif' src={ImagePlaceholder}></img>
+                <img className='project-component-gif' src={SortGif}></img>
                 <div className='project-component-info-container'>
-                    <header className='project-component-title project-component-title-3'>Lorem</header>
+                    <header className='project-component-title project-component-title-3'>Sorting Algorithm Visualizer</header>
                     <p className='project-component-overview'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
-                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        An application that implements java swing to draw out a visual of the process of loop sorting. 
+                        There are interactive buttons to visualize different sorting algorithms and a slider to change 
+                        the delay in between swaps. The height of each bar corresponds to its value and its x position 
+                        corresponds to it's index in the array.
                     </p>
-                    <a className='github-link'><i className="fa-brands fa-github" target='_blank'></i> Github <i class="fa-solid fa-arrow-up-right-from-square icon"></i></a>
+                    <a href='https://github.com/ahmad-masud/Sorting-Algorithm-Visualizer' className='github-link' target='_blank'><i className="fa-brands fa-github"></i> Github <i class="fa-solid fa-arrow-up-right-from-square icon"></i></a>
                     <ul className='stacks'>
-                        <li className='stack'>Lorem</li>
-                        <li className='stack'>Ipsum</li>
+                        <li className='stack'>Java</li>
+                        <li className='stack'>Java Swing</li>
                     </ul>
                 </div>
             </div>
