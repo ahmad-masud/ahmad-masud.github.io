187e393aa1fcece71e1d81321272bcc7469cea9c
Ahmad Masud <ahmadmasud25@hotmail.com>
2022-12-27 18:55:52 -0800

    commit build to gh-pages

--- src/App.js
+++ src/App.js
@@ -8,20 +8,21 @@ import Projects from './Pages/Projects.js';
 import Education from './Pages/Education.js';
 import Contact from './Pages/Contact.js';
 import { Routes, Route, useLocation } from 'react-router-dom';
+import ScrollToTop from './Components/ScrollToTop/ScrollToTop.js'
 
 function App() {
   return (
     <div className='app'>
       <Navbar />
       <div className='Content'>
-          <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/About' element={<About />} />
-            <Route exact path='/Experiences' element={<Experiences />} />
-            <Route exact path='/Projects' element={<Projects />} />
-            <Route exact path='/Education' element={<Education />} />
-            <Route exact path='/Contact' element={<Contact />} />
-          </Routes>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/About' element={<About />} />
+          <Route exact path='/Experiences' element={<Experiences />} />
+          <Route exact path='/Projects' element={<Projects />} />
+          <Route exact path='/Education' element={<Education />} />
+          <Route exact path='/Contact' element={<Contact />} />
+        </Routes>
       </div>
       <Footer />
     </div>
--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+import ScrollToTop from './Components/ScrollToTop/ScrollToTop.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
