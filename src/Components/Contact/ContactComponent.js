556391010c313b047b178cc507fed8af968489ac
Ahmad Masud <96448477+ahmad-masud@users.noreply.github.com>
2023-01-15 18:54:35 -0800

    update

--- src/Components/Contact/ContactComponent.js
+++ src/Components/Contact/ContactComponent.js
@@ -25,12 +25,12 @@ function ContactComponent() {
         <div className='contact'>
             <label className='contact-success-label' id='success-label'>Sent Successfully <i class="fa-solid fa-circle-check"></i></label>
             <form className='contact-form' ref={form} onSubmit={sendEmail}>
-                <label className='contact-name-label'>Name</label>
+                <label className='contact-name-label required'>Name</label>
                 <input className='contact-name-input' type="text" name="from_name" required />
-                <label className='contact-email-label'>Email</label>
+                <label className='contact-email-label required'>Email</label>
                 <input className='contact-email-input' type="email" name="from_email" required/>
-                <label className='contact-message-label'>Message</label>
-                <textarea className='contact-message' name="message" />
+                <label className='contact-message-label required'>Message</label>
+                <textarea className='contact-message' name="message" required/>
                 <input className='contact-submit' type="submit" value="Send" required/>
             </form>  
         </div>
