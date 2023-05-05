0841c9db71687f80c6ff7f1109a8919fa37d30fb
ahmad-masud <ahmadmasud25@hotmail.com>
2023-05-05 01:21:34 -0700

    added dark mode preference

--- src/Components/Info/Info.js
+++ src/Components/Info/Info.js
@@ -7,14 +7,14 @@ function Info() {
         <header class="info-main-header">Nice to Meet You</header>
         <p class='info-paragraph'>
           <span>
-            Hello! My name is Ahmad Masud and I am a motivated and dedicated individual currently pursuing my education at Simon 
+            Hello! My name is <span className='highlight'>Ahmad Masud</span> and I am a motivated and dedicated individual currently pursuing my education at Simon 
             Fraser University. I thrive in a professional environment that encourages learning and fosters collaboration to solve 
             complex challenges efficiently and effectively.
           </span>
           <span>
-            As an aspiring software engineer, I am deeply passionate about computer programming and constantly strive to innovate 
+            As an aspiring <span className='highlight'>software engineer</span>, I am deeply passionate about computer programming and constantly strive to innovate 
             and create solutions that make a positive impact on the world. I believe in the power of creativity and critical 
-            problem-solving skills, and I am committed to leveraging these attributes to excel in the field of Computer Science.
+            problem-solving skills, and I am committed to leveraging these attributes to excel in the field of <span className='highlight'>Computer Science.</span>
           </span>
         </p>
       </div>
