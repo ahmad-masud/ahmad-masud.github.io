04a200db8364680fa1bfb27dd0e4152046fa83d3
ahmad-masud <ahmadmasud25@hotmail.com>
2023-05-26 00:11:21 -0700

    added an experience

---
+++ src/ExperiencesData/Kumon/index.js
@@ -0,0 +1,17 @@
+import KumonImage from './Kumon.jpg'
+
+const Kumon = {
+    image: KumonImage,
+    date: "05/2023 - CURRENT",
+    title: "Math/Reading Tutor ⋅ Kumon",
+    description:
+        `I started working at kumon in May of 2023. I grade students math and reading work such as calculus, algebra, poetry writting,
+        and reading. I also teach younger children how to read words and build their foundation of the english language `,
+    listItems: [
+        `Facilitated self-learning to students, adapting to individual needs and pace, fostering self-reliance and confidence`,
+        `Assisted students in mastering and advancing through incremental levels of math and reading curriculum`,
+        `Graded students' assignments promptly and accurately, providing constructive feedback to support improvement`,
+    ],
+}
+
+export default Kumon;
