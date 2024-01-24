a76c7d6742e7ad8ac74babac32d7a4525b44fc70
ahmad-masud <ahmadmasud25@hotmail.com>
2024-01-24 11:11:58 -0800

    Added File Sorter to Projects

---
+++ src/ProjectsData/4-File-Sorter/index.js
@@ -0,0 +1,15 @@
+import FileSorterImage from './File-Sorter.png'
+
+const FileSorter = {
+    title: "File Sorter",
+    description: 
+        `The File Sorter Application is a Python-based tool designed to help organize files in a directory. 
+        It automatically sorts files into subdirectories 
+        based on chosen criteria: file size, file type, or date modified.`,
+    github: "https://github.com/ahmad-masud/File-Sorter",
+    stacks: ["Python"],
+    image: FileSorterImage,
+    frontpage: false
+}
+
+export default FileSorter
